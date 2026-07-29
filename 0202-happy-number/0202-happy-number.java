4d71e2c734fd4a855341a0506609363fb9b92df0
harini005-kavin <harinikbiomathsem@gmail.com>
2026-07-29 14:01:51 +0530

    Time: 1 ms (77.31%), Space: 40.5 MB (100%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while(n!=1&&n!=4) {
+            int sum = 0;
+            while (n != 0) {
+              int  mod = n % 10;
+                sum = sum + mod * mod;
+                n = n / 10;
+            }
+            n = sum;
+        }
+if(n==1)
+{
+    return true;
+}
+return false;
+    }
+}
